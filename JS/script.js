1d5b0cbc073304db2f455c5eab44f5cc3641c6e9
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2022-10-11 17:46:26 -0300

    alterações

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
