6eb7584593509070785ce97d3bc7237c95c6840e
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2022-11-10 19:20:22 -0300

    Começando as funççoes

--- JS/script.js
+++ JS/script.js
@@ -3,13 +3,37 @@ const currentOperationText = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
 
 class Calculator{
+  constructor(previousOperationText,currentOperationText){
+    this.previousOperationText = previousOperationText
+    this.currentOperationText = currentOperationText
+    this.currentOperation = ""
+  }
+//metodo adiciona u digito
+  addDigit(digit){
+    
+    this.currentOperation = digit
+    this.updateScreen()
+  }
 
+  updateScreen(){
+    this.currentOperationText.innerText += this.currentOperation
+    
+  }
+  
 }
 
-buttons.forEach((btn) =>{
-    btn.addEventListener("click", (e) =>{
+const calc = new Calculator(previousOperationText, currentOperationText)
 
+buttons.forEach((btn) =>{
+    btn.addEventListener("click", (e) => {
         const value =e.target.innerText
-        console.log(value)
+
+        if(+value >= 0 || value === "."){ 
+          calc.addDigit(value);
+
+        }else{
+          console.log("OP:"+ value);
+        }
+        
     })
 })
