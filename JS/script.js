d4a5a41caf415fc4f8f1913aabdb8403b30596d2
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-01-23 09:38:34 -0300

    Finalizando calculadora

--- JS/script.js
+++ JS/script.js
@@ -4,42 +4,133 @@ const buttons = document.querySelectorAll("#buttons-container button");
 
 class Calculator{
   constructor(previousOperationText,currentOperationText){
-    this.previousOperationText = previousOperationText
-    this.currentOperationText = currentOperationText
-    this.currentOperation = ""
+    this.previousOperationText = previousOperationText;
+    this.currentOperationText = currentOperationText;
+    this.currentOperation = "";
   }
-//add digit
+//add digito
   addDigit(digit){
-    //check if current operation already has dot
+    //ver se a operação atual já tem ponto
     if(digit === "." && this.currentOperationText.innerText.includes(".")){
       return;     //Evita de repetir o "."
     }
-
-    this.currentOperation = digit
-    this.updateScreen()
+    this.currentOperation = digit;
+    this.updateScreen();
   }
 
-  //   Process all calculator  operations
+  //   Processar todas as operações da calculadora
 processOperation(operation){
+
   console.log(operation)
-  //  Get current and previous  value
+  //  Verifique se o valor atual está vazio
+  if (this.currentOperationText.innerText === "" && operation !== "C") {
+    // Change operation
+    if (this.previousOperationText.innerText !== "") {
+      this.changeOperation(operation);
+    }
+    return;
+  }
+
+  //  Obter valor atual e anterior
   let operationValue
-  let previous = +this.previousOperationText.innerText;
+  let previous = +this.previousOperationText.innerText.split(" ")[0];
   let current = +this.currentOperationText.innerText;
 
   switch (operation) {
     case "+":
+      operationValue = previous + current;
+      this.updateScreen(operationValue, operation, current, previous);
+      break;
+    case "-":
+      operationValue = previous - current;
+      this.updateScreen(operationValue, operation, current, previous);
+      break;
+    case "*":
+      operationValue = previous * current;
+      this.updateScreen(operationValue, operation, current, previous);
+      break;
+    case "/":
+      operationValue = previous / current;
+      this.updateScreen(operationValue, operation, current, previous);
+      break;
+    case "DEL":
+      this.processDelOperator();
+      break;
+    case "CE":
+      this.processClearCurrentOperator();
+      break;
+    case "C":
+      this.processClearOperator();
+      break;
+    case "=":
+      this.processEqualOperator();
       break;
     default:
       return;
   }
 }
-// Change values of the calculator screen 
-  updateScreen(operationValue = null, operation = null , current = null){
-    this.currentOperationText.innerText += this.currentOperation
+
+// Alterar valores da tela da calculadora
+updateScreen(
+  operationValue = null,
+  operation = null,
+  current = null,
+  previous = null
+) {
+  if (operationValue === null) {
+
+    //  Anexar número ao valor atual
+    this.currentOperationText.innerText += this.currentOperation;
+  } else {
+
+    //  ver se o valor é zero, se for apenas adicione o valor atual
+    if (previous === 0) {
+      operationValue = current;
+    }
+    
+    // adicionar valor atual ao anterior
+    this.previousOperationText.innerText = `${operationValue} ${operation}`;
+    this.currentOperationText.innerText = "";
   }
 }
 
+// Altera a operação  
+  changeOperation(operation) {
+    const mathOperations = ["*", "-", "+", "/"];
+
+    if (!mathOperations.includes(operation)) {
+      return;
+    }
+
+    this.previousOperationText.innerText =
+      this.previousOperationText.innerText.slice(0, -1) + operation;
+  }
+
+  // Deleta um digito
+  processDelOperator() {
+    this.currentOperationText.innerText =
+      this.currentOperationText.innerText.slice(0, -1);
+  }
+
+  // limpa operação
+  processClearCurrentOperator() {
+    this.currentOperationText.innerText = "";
+  }
+
+  // Limpa todas operações 
+  processClearOperator() {
+    this.currentOperationText.innerText = "";
+    this.previousOperationText.innerText = "";
+  }
+
+  //  Processar uma operação
+  processEqualOperator() {
+    let operation = this.previousOperationText.innerText.split(" ")[1];
+
+    this.processOperation(operation);
+  }
+}
+// 
 const calc = new Calculator(previousOperationText, currentOperationText)
 
 buttons.forEach((btn) =>{
--- README.md
+++ README.md
@@ -9,4 +9,5 @@
 - CSS
 - Javascript
 
-## Em PRODUÇÃO . . .
+## Projeto do Matheus Battisti - Hora de Codar:https://www.youtube.com/watch?v=KCfaPZ2t2yA&list=WL&index=28&t=2177s (Neste vídeo você vai construir do zero uma calculadora orientada a objetos com #HTML #CSS e #JavaScript. A inspiração do projeto foi a calculadora do Windows.
+Trabalharemos com boas práticas, utilizando #Flexbox, #Grid, #POO e também interação através do DOM para capturar os eventos do HTML no JS)
